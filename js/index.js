a917be49bc02ed3ad97f8ef0413beb402554a941
Bryl <111213812+Brylrio@users.noreply.github.com>
2026-09-04 16:41:04 +0800

    add javascript for toggle

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav_links");
 
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
+
+// optional: auto-close menu when a link is clicked
+navLinks.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+  });
+});
